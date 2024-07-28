c7350215dcf5fe39542937b1105ec00770d47920
parag0712 <paragvadgama123@gmail.com>
2024-07-28 21:50:59 +0530

    delete api done

--- blog/main.py
+++ blog/main.py
@@ -49,9 +49,30 @@ def get_single_blog(id,response:Response,db:Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Not Found")
     return blog
 
+@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
+def update_blog(id,request:schemas.Blog,db:Session = Depends(get_db)):
+    try:
+        blog = db.query(models.Blog).filter(models.Blog.id == id)
+        if not blog.first():
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"blog not found")
+        blog.update({
+            'title':request.title,
+            'body':request.body
+        })
+        db.commit()
+        return {'message':"blog updated"}
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=500, detail=f"Error creating blog {e}")
+
 @app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
 def delete_blog(id,db:Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"blog not found")
+    db.commit() 
+    blog.delete(synchronize_session=False)
     db.commit()
     return {'message':f"blog delete successfully"}
     

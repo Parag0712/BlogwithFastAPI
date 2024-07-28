778c43bf7815ae5c143499fae8e8750cccfcd08a
parag0712 <paragvadgama123@gmail.com>
2024-07-28 17:53:50 +0530

    create blog done

--- blog/main.py
+++ blog/main.py
@@ -1,12 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends,HTTPException
 from blog import models,schemas,database
-
+from sqlalchemy.orm import Session
 # uvicorn main:app --reload
 # pip install -r requirements.txt
 # https://docs.sqlalchemy.org/en/20/orm/tutorial.html
+# FastAPI use this orm https://docs.sqlalchemy.org/en/20/orm/quickstart.html#create-an-engine
 app = FastAPI()
 
+# for bind model to db 
 models.Base.metadata.create_all(database.engine)
+
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.get("/")
-def index(req:schemas.Blog):
+def index():
     return "app running"
+
+@app.post("/blog")
+def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
+    try:
+        new_blog = models.Blog(title=request.title, body=request.body)
+        db.add(new_blog)
+        db.commit()
+        db.refresh(new_blog)
+        return new_blog
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=500, detail=f"Error creating blog {e}")
--- blog/models.py
+++ blog/models.py
@@ -6,14 +6,3 @@ class Blog(Base):
     id = Column(Integer,primary_key=True,index=True)
     title = Column(String)
     body = Column(String)
-    creator = relationship("Users",back_populates="blogs")
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True,index=True)
-    name = Column(String)
-    email = Column(String)
-    password = Column(String)    
-    blogs = relationship("Blog",back_populates = "creator")
-

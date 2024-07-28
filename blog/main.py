f596d3795079a1d58ce0db3da34b80530ba251db
parag0712 <paragvadgama123@gmail.com>
2024-07-28 22:33:16 +0530

    response model done

--- blog/main.py
+++ blog/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI,Depends,HTTPException,status,Response
 from blog import models,schemas,database
 from sqlalchemy.orm import Session
+from typing import List
 # uvicorn main:app --reload
 # pip install -r requirements.txt
 # https://docs.sqlalchemy.org/en/20/orm/tutorial.html
@@ -10,6 +11,7 @@ app = FastAPI()
 # for bind model to db 
 models.Base.metadata.create_all(database.engine)
 
+# get connection
 def get_db():
     db = database.SessionLocal()
     try:
@@ -17,14 +19,21 @@ def get_db():
     finally:
         db.close()
 
+# for home route
 @app.get("/")
 def index():
     return "app running"
 
 # add new blog 
+# for db session we use Depends
+# for get data from user we use schemas request:Blog
+# for exeception we send HTTPExeception
 @app.post("/blog", status_code=status.HTTP_201_CREATED)
 def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     try:
+        # here query for add data 
+        # sqlalchemy orm used by fastAPI
+        # we send data
         new_blog = models.Blog(title=request.title, body=request.body)
         db.add(new_blog)
         db.commit()
@@ -35,12 +44,15 @@ def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
         raise HTTPException(status_code=500, detail=f"Error creating blog {e}")
 
 # for get all blogs
-@app.get("/blog",status_code=status.HTTP_200_OK)
+@app.get("/blog",status_code=status.HTTP_200_OK,response_model=List[schemas.ShowBlog])    
 def all(db:Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get("/blog/{id}",status_code=200)
+# for custom response we use response
+# for short cut we use 404
+# using response model we can specify which type of response data we send
+@app.get("/blog/{id}",status_code=200,response_model=schemas.ShowBlog)
 def get_single_blog(id,response:Response,db:Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -49,22 +61,27 @@ def get_single_blog(id,response:Response,db:Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Not Found")
     return blog
 
+# for update we use put request 
 @app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
 def update_blog(id,request:schemas.Blog,db:Session = Depends(get_db)):
     try:
+        # here query for find data
         blog = db.query(models.Blog).filter(models.Blog.id == id)
         if not blog.first():
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"blog not found")
+        # update data
         blog.update({
             'title':request.title,
             'body':request.body
         })
+        # commit
         db.commit()
         return {'message':"blog updated"}
     except Exception as e:
         db.rollback()
         raise HTTPException(status_code=500, detail=f"Error creating blog {e}")
 
+# for status we use status
 @app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
 def delete_blog(id,db:Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
@@ -72,7 +89,7 @@ def delete_blog(id,db:Session = Depends(get_db)):
     if not blog.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"blog not found")
     db.commit() 
-    blog.delete(synchronize_session=False)
+    blog.delete(synchronize_session=False) 
     db.commit()
     return {'message':f"blog delete successfully"}
     
